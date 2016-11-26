60967add93459394a1d1a31b05c4171dcfcc7be5
Ingmar Delsink <ingmardelsink@hotmail.com>
2016-11-27 00:42:58 +0100

    add some extra verbosity

--- root/scripts/entrypoint.sh
+++ root/scripts/entrypoint.sh
@@ -5,31 +5,35 @@ CONFIGURED_APACHE=/var/ampache-docker/configured/apache
 CONFIGURED_MYSQL=/var/ampache-docker/configured/mysql
 CONFIGURED_AMPACHE=/var/ampache-docker/configured/ampache
 
-echo "~~ configuring system if not yet configured ~~"
+echo "~~ Configuring system if not yet configured ~~"
 # supervisord
 if [ ! -f "${CONFIGURED_SUPERVISORD}" ]; then
+    echo "~~ Configuring supervisord ~~"
     /scripts/configure/supervisord.sh && \
     mkdir -p ${CONFIGURED_SUPERVISORD%/*} && touch "${CONFIGURED_SUPERVISORD}"
 fi
 # apache
 if [ ! -f "${CONFIGURED_APACHE}" ]; then
+    echo "~~ Configuring apache ~~"
     /scripts/configure/apache.sh && \
     mkdir -p ${CONFIGURED_APACHE%/*} && touch "${CONFIGURED_APACHE}"
 fi
 
 # mysql
 if [ ! -f "${CONFIGURED_MYSQL}" ]; then
+    echo "~~ Configuring mysql ~~"
     /scripts/configure/mysql.sh && \
     mkdir -p ${CONFIGURED_MYSQL%/*} && touch "${CONFIGURED_MYSQL}"
 fi
 
-# mysql
+# ampache
 if [ ! -f "${CONFIGURED_AMPACHE}" ]; then
+    echo "~~ Configuring ampache ~~"
     /scripts/configure/ampache.sh && \
     mkdir -p ${CONFIGURED_AMPACHE%/*} && touch "${CONFIGURED_AMPACHE}"
 fi
 
 
 # start supervisord
-echo "~~ starting the service manager supervisord ~~"
+echo "~~ Starting the service manager supervisord ~~"
 /usr/bin/supervisord -c /etc/supervisor/supervisord.conf
