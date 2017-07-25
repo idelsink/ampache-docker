15af84be7b52f514ef87740730b9ab3964d98397
Ingmar Delsink <ingmardelsink@hotmail.com>
2017-07-26 00:03:34 +0200

    Move checking of first time setup

--- root/scripts/entrypoint.sh
+++ root/scripts/entrypoint.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env sh
 
-echo "~~ Configuring system if not yet configured ~~"
-
-/scripts/first-time-setup.sh
+# Configuring system if not yet configured
+FIRST_TIME_SETUP_DONE=/var/ampache-docker/first-time-setup
+if [ ! -f "${FIRST_TIME_SETUP_DONE}" ]; then
+    /scripts/first-time-setup.sh && \
+    mkdir -p "${FIRST_TIME_SETUP_DONE%/*}" && touch "${FIRST_TIME_SETUP_DONE}"
+fi
 
 # start supervisord
 echo "~~ Starting the service manager supervisord ~~"
--- root/scripts/first-time-setup.sh
+++ root/scripts/first-time-setup.sh
@@ -2,12 +2,6 @@
 
 # Run the first time setup
 # (filling in the parameters that can change)
-
-FIRST_TIME_SETUP_DONE=/var/ampache-docker/first-time-setup
-
-if [ ! -f "${FIRST_TIME_SETUP_DONE}" ]; then
-    echo "~~ Running first-time-setup ~~"
-    # Run all the scripts from given directory
-    run-parts /scripts/first-time-setup/ && \
-    mkdir -p "${FIRST_TIME_SETUP_DONE%/*}" && touch "${FIRST_TIME_SETUP_DONE}"
-fi
+echo "~~ Running first-time-setup ~~"
+# Run all the scripts from given directory
+run-parts /scripts/first-time-setup/
