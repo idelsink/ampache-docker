0e0c072c0fb50d2d3a68b64824c52cc1826a8c3e
Ingmar Delsink <ingmardelsink@hotmail.com>
2017-07-27 00:17:14 +0200

    Set IP Addres on apache startup

--- root/scripts/start/apache.sh
+++ root/scripts/start/apache.sh
@@ -5,4 +5,8 @@
 # delete PID file (if old one exists)
 rm -f "${APACHE_PID_FILE}"
 
+# Set IP Address on startup
+IP_ADDR=$(ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p')
+sed -i "s@\(^ServerName\).*@\ServerName ${IP_ADDR}@g" /etc/apache2/httpd.conf
+
 httpd "${@}"
