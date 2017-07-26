038947e15e3290fc73dd9a748afecf757d5f0c43
Ingmar Delsink <ingmardelsink@hotmail.com>
2017-07-26 22:00:44 +0200

    Clean up a few comments and some 'echo' statements

--- root/scripts/first-time-setup.sh
+++ root/scripts/first-time-setup.sh
@@ -2,6 +2,6 @@
 
 # Run the first time setup
 # (filling in the parameters that can change)
-echo "~~ Running first-time-setup ~~"
+
 # Run all the scripts from given directory
 run-parts /scripts/first-time-setup/
